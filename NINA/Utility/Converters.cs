28341cc55741cf50c3007f3bf352a4a850f3d78d
Isbeorn <isbeorn86@googlemail.com>
2017-01-19 01:31:14 +0100

    Fixed ui bug in PA view; Hide Image Manipulation - does not work properly anyway
    Former-commit-id: 141f50d0d7242d3fc9fb183bb1fa6e77378a8d62
    Former-commit-id: e6c03ebaa21215579947e29512a10161c45cfae3

--- NINA/Utility/Converters.cs
+++ NINA/Utility/Converters.cs
@@ -66,6 +66,16 @@ namespace NINA.Utility {
         }
     }
 
+    public class NullToVisibilityCollapsedConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+
     public class InverseNullToBooleanConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             return !(value == null);
