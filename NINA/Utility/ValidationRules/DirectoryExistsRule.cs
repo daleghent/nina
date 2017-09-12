60ffe8927176da2b1172031b308177eafc2ceee9
Isbeorn <isbeorn86@googlemail.com>
2017-09-13 00:20:27 +0200

    Added more validation rules for input
    Former-commit-id: 6a148094e7c29dbd6f1c696621dbc0059c1a8037
    Former-commit-id: 012d9b2371cd7c8cff138842b03884428eea98d7

---
+++ NINA/Utility/ValidationRules/DirectoryExistsRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NINA.Utility.ValidationRules
+{
+    public class DirectoryExistsRule : ValidationRule {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+            var dir = value.ToString();
+            if(!Directory.Exists(dir)) {
+                return new ValidationResult(false, "Invalid Directory");
+            } else {
+                return new ValidationResult(true, null);
+            }
+        }
+    }
+}
---
+++ NINA/Utility/ValidationRules/FileExistsRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NINA.Utility.ValidationRules
+{
+    public class FileExistsRule : ValidationRule {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+            var dir = value.ToString();
+            if (!File.Exists(dir)) {
+                return new ValidationResult(false, "Invalid File");
+            } else {
+                return new ValidationResult(true, null);
+            }
+        }
+    }
+}
