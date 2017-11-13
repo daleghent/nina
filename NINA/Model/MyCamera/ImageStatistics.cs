6c07eb25ac66d0bc8f448470a426ff1920c84b30
Berg, Stefan <stefan.berg@gmx.net>
2017-11-13 13:27:27 +0100

    Move ImageArray and Statistics into separate files.
    Former-commit-id: be8c386e4aab93756fb6c6d28fabfb4092429909
    Former-commit-id: 1bb287030764826e1596914b203f25b08a8f7947

---
+++ NINA/Model/MyCamera/ImageStatistics.cs
@@ -0,0 +1,48 @@
+﻿using NINA.Utility;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NINA.Model.MyCamera {
+    public class ImageStatistics : BaseINPC {
+        public int Id { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public double StDev { get; set; }
+        public double Mean { get; set; }
+        private double _hFR;
+
+        public IEnumerable Histogram { get; set; }
+        public static double HistogramMajorStep = 642.5;
+        public static double HistogramMinorStep = 321.25;
+        public static double HistogramResolution = 1285;
+
+        public int DetectedStars {
+            get {
+                return _detectedStars;
+            }
+
+            set {
+                _detectedStars = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public double HFR {
+            get {
+                return _hFR;
+            }
+
+            set {
+                _hFR = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int _detectedStars;
+
+    }
+}
