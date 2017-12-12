10a9bb400cb18e4f8e1a1bd79d2600f4e9138553
Isbeorn <isbeorn86@googlemail.com>
2017-12-12 01:31:32 +0100

    Autoscroll thumbnail content when new item is added and scrolled to the right.
    Former-commit-id: 08969a65034975dcc123ab8899aba04bcbc7271d
    Former-commit-id: 4b5c13fdfc6603a28aa3bbd927981e4a888cf646

--- NINA/View/ThumbnailListView.xaml.cs
+++ NINA/View/ThumbnailListView.xaml.cs
@@ -21,5 +21,29 @@ namespace NINA.View {
         public ThumbnailListView() {
             InitializeComponent();
         }
+
+        private bool _autoScroll = true;
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e) {
+            // User scroll event : set or unset auto-scroll mode
+            if (e.ExtentWidthChange == 0) {   // Content unchanged : user scroll event
+                if (ScrollViewer.HorizontalOffset == ScrollViewer.ScrollableWidth) {
+                    // Scroll bar is most right position
+                    // Set autoscroll mode
+                    _autoScroll = true;
+                } else {
+                    // Scroll bar isn't in most right position
+                    // Unset auto-scroll mode
+                    _autoScroll = false;
+                }
+            }
+
+            // Content scroll event : auto-scroll eventually
+            if (_autoScroll && e.ExtentWidthChange != 0) {
+                // Content changed and auto-scroll mode set
+                // Autoscroll
+                ScrollViewer.ScrollToHorizontalOffset(ScrollViewer.ExtentWidth);
+            }
+        }
     }
 }
