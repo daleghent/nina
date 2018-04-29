43568d48c3ab0f5c86cd65c6a372d417e1c598d9
darkarchon <root.tarsonis@googlemail.com>
2018-04-29 13:56:59 +0200

    more style stuff
    Former-commit-id: 106bad0185ab77133cc6a64bbc4bcd021a556e19
    Former-commit-id: 9008bf46e71b96ee0e9ffebf01b73778897e202c

--- NINA/ViewModel/EquipmentChooserVM.cs
+++ NINA/ViewModel/EquipmentChooserVM.cs
@@ -4,16 +4,12 @@ using System;
 using System.Linq;
 using System.Windows.Input;
 
-namespace NINA.ViewModel
-{
-    internal abstract class EquipmentChooserVM : BaseVM
-    {
-        public EquipmentChooserVM(Type equipmentType)
-        {
+namespace NINA.ViewModel {
+    internal abstract class EquipmentChooserVM : BaseVM {
+        public EquipmentChooserVM(Type equipmentType) {
             SetupDialogCommand = new RelayCommand(OpenSetupDialog);
             GetEquipment();
-            Mediator.Instance.RegisterRequest(new GetEquipmentNameByIdMessageHandle(equipmentType, (GetEquipmentNameByIdMessage msg) =>
-            {
+            Mediator.Instance.RegisterRequest(new GetEquipmentNameByIdMessageHandle(equipmentType, (GetEquipmentNameByIdMessage msg) => {
                 var name = Devices.SingleOrDefault(dev => dev.Id == msg.Id).Name;
                 return name;
             }));
@@ -21,18 +17,14 @@ namespace NINA.ViewModel
 
         private AsyncObservableCollection<Model.IDevice> _devices;
 
-        public AsyncObservableCollection<Model.IDevice> Devices
-        {
-            get
-            {
-                if (_devices == null)
-                {
+        public AsyncObservableCollection<Model.IDevice> Devices {
+            get {
+                if (_devices == null) {
                     _devices = new AsyncObservableCollection<Model.IDevice>();
                 }
                 return _devices;
             }
-            set
-            {
+            set {
                 _devices = value;
             }
         }
@@ -41,14 +33,11 @@ namespace NINA.ViewModel
 
         private Model.IDevice _selectedDevice;
 
-        public Model.IDevice SelectedDevice
-        {
-            get
-            {
+        public Model.IDevice SelectedDevice {
+            get {
                 return _selectedDevice;
             }
-            set
-            {
+            set {
                 _selectedDevice = value;
                 RaisePropertyChanged();
             }
@@ -56,25 +45,18 @@ namespace NINA.ViewModel
 
         public ICommand SetupDialogCommand { get; private set; }
 
-        private void OpenSetupDialog(object o)
-        {
-            if (SelectedDevice?.HasSetupDialog == true)
-            {
+        private void OpenSetupDialog(object o) {
+            if (SelectedDevice?.HasSetupDialog == true) {
                 SelectedDevice.SetupDialog();
             }
         }
 
-        public void DetermineSelectedDevice(string id)
-        {
-            if (Devices.Count > 0)
-            {
+        public void DetermineSelectedDevice(string id) {
+            if (Devices.Count > 0) {
                 var items = (from device in Devices where device.Id == id select device);
-                if (items.Count() > 0)
-                {
+                if (items.Count() > 0) {
                     SelectedDevice = items.First();
-                }
-                else
-                {
+                } else {
                     SelectedDevice = Devices.First();
                 }
             }
