31b10d920c90e271fdb05256a6927cb383a0b0c1
Isbeorn <isbeorn86@googlemail.com>
2018-05-11 16:28:36 +0200

    Refactored RMS calculation into own object. added unit tests for same.
    Former-commit-id: ed33d65915e3e69ec1e99145895f29c86ebdb7dd
    Former-commit-id: 083fa400b79902f62bec3a4069e79ac434fa8b6f

--- NINA/ViewModel/GuiderVM.cs
+++ NINA/ViewModel/GuiderVM.cs
@@ -1,4 +1,5 @@
 ﻿using NINA.Model.MyGuider;
+using NINA.Model;
 using NINA.Utility;
 using NINA.Utility.Enum;
 using NINA.Utility.Mediator;
@@ -33,6 +34,7 @@ namespace NINA.ViewModel {
 
             GuideStepsHistory = new AsyncObservableLimitedSizedStack<IGuideStep>(HistorySize);
             GuideStepsHistoryMinimal = new AsyncObservableLimitedSizedStack<IGuideStep>(MinimalHistorySize);
+            RMS = new RMS();
 
             RegisterMediatorMessages();
         }
@@ -155,6 +157,7 @@ namespace NINA.ViewModel {
         private async Task<bool> Connect() {
             GuideStepsHistory.Clear();
             GuideStepsHistoryMinimal.Clear();
+            RMS.Clear();
             Guider = new PHD2Guider(profileService);
             Guider.PropertyChanged += Guider_PropertyChanged;
             return await Guider.Connect();
@@ -163,6 +166,7 @@ namespace NINA.ViewModel {
         private bool Disconnect() {
             GuideStepsHistory.Clear();
             GuideStepsHistoryMinimal.Clear();
+            RMS.Clear();
             var discon = Guider.Disconnect();
             Guider = null;
             return discon;
@@ -179,83 +183,20 @@ namespace NINA.ViewModel {
                 }
                 GuideStepsHistoryMinimal.Add(step);
                 GuideStepsHistory.Add(step);
-            }
+                RMS.AddDataPoint(step.RADistanceRaw, step.DecDistanceRaw);
+            }            
+        }       
 
-            // really, that's how phd2 does it and I don't want to change it
-            int n = GuideStepsHistory.Count;
-            var items = GuideStepsHistory.Select(item => new Tuple<double, double>(item.RADistanceRaw, item.DecDistanceRaw));
-            var itemsRa = items.Select(item => item.Item1);
-            var itemsDec = items.Select(item => item.Item2);
-
-            double sum_y2 = itemsRa.Sum(y => y * y);
-            double sum_y = itemsRa.Sum(y => y);
-
-            RMSRA = Math.Sqrt(n * sum_y2 - sum_y * sum_y) / n;
-
-            sum_y2 = itemsDec.Sum(y => y * y);
-            sum_y = itemsDec.Sum(y => y);
-
-            RMSDec = Math.Sqrt(n * sum_y2 - sum_y * sum_y) / n;
-            RMSTotal = Math.Sqrt((Math.Pow(RMSRA, 2) + Math.Pow(RMSDec, 2)));
-
-            if (GuiderScale == GuiderScaleEnum.ARCSECONDS) {
-                RMSRA *= PixelScale;
-                RMSDec *= PixelScale;
-                RMSTotal *= PixelScale;
-            }
-        }
-
-        public double RMSRA {
+        private RMS rms;
+        public RMS RMS {
             get {
-                return _rmsRA;
+                return rms;
             }
             set {
-                _rmsRA = value;
+                rms = value;
                 RaisePropertyChanged();
             }
-        }
-
-        public double RMSDec {
-            get {
-                return _rmsDec;
-            }
-            set {
-                _rmsDec = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        public double RMSTotal {
-            get {
-                return _rmsTotal;
-            }
-            set {
-                _rmsTotal = value;
-                RaiseAllPropertiesChanged();
-            }
-        }
-
-        public string RMSRAText {
-            get {
-                return string.Format(Locale.Loc.Instance["LblPHD2RARMS"], RMSRA.ToString("0.00"));
-            }
-        }
-
-        public string RMSDecText {
-            get {
-                return string.Format(Locale.Loc.Instance["LblPHD2DecRMS"], RMSDec.ToString("0.00"));
-            }
-        }
-
-        public string RMSTotalText {
-            get {
-                return string.Format(Locale.Loc.Instance["LblPHD2TotalRMS"], RMSTotal.ToString("0.00"));
-            }
-        }
-
-        private double _rmsTotal;
-        private double _rmsRA;
-        private double _rmsDec;
+        }        
 
         private void ConvertStepToArcSec(IGuideStep pixelStep) {
             // only displayed values are changed, not the raw ones
@@ -293,6 +234,9 @@ namespace NINA.ViewModel {
                         ConvertStepToPixels(s);
                     }
                 }
+
+                RMS.SetScale(GuiderScale == GuiderScaleEnum.ARCSECONDS ? PixelScale : 1);
+
                 RaisePropertyChanged(nameof(GuideStepsHistory));
                 RaisePropertyChanged(nameof(GuideStepsHistoryMinimal));
             }
