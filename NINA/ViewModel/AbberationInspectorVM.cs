98f989ed7976d553ca679b84ce40d7fe83bf4886
Isbeorn <isbeorn86@googlemail.com>
2018-08-25 02:57:05 +0200

    Draw grid lines to aberration inspector image
    Former-commit-id: c7e678af2c9673840e2d5a15af681f08a8460d2b
    Former-commit-id: 1d510f63f1e17b02931ff6e8a7f4d4db7289d9bb

--- NINA/ViewModel/AbberationInspectorVM.cs
+++ NINA/ViewModel/AbberationInspectorVM.cs
@@ -14,43 +14,76 @@ namespace NINA.ViewModel {
 
         public AbberationInspectorVM(IProfileService profileService, BitmapSource source) : base(profileService) {
             Columns = 3;
+            SeparationSize = 4;
             CellSize = 256;
             DetermineImage(source);
         }
 
         private void DetermineImage(BitmapSource source) {
-            var panelSize = cellSize * columns;
+            var panelSize = CellSize * Columns + SeparationSize * Columns;
 
             if (source.Width < panelSize || source.Height < panelSize) {
                 throw new Exception(string.Format("Image too small for aberration inspector. Must be at least {0}x{0}", panelSize));
             }
 
-            var widthOffset = (source.Width - columns * cellSize) / (columns - 1);
-            var heightOffset = (source.Height - columns * cellSize) / (columns - 1);
+            var widthOffset = (source.Width - Columns * CellSize) / (Columns - 1);
+            var heightOffset = (source.Height - Columns * CellSize) / (Columns - 1);
 
             DrawingVisual drawingVisual = new DrawingVisual();
-            var pen = new Pen() { Brush = Brushes.Black, Thickness = 1 };
+            var pen = new Pen() { Brush = Brushes.Black, Thickness = SeparationSize };
             using (DrawingContext drawingContext = drawingVisual.RenderOpen()) {
-                for (int column = 0; column < columns; column++) {
-                    for (int row = 0; row < columns; row++) {
+                for (int column = 0; column < Columns; column++) {
+                    for (int row = 0; row < Columns; row++) {
                         var rect = new Int32Rect(
-                            (int)(column * widthOffset + column * cellSize),
-                            (int)(row * heightOffset + row * cellSize),
-                            cellSize,
-                            cellSize
+                            (int)(column * widthOffset + column * CellSize),
+                            (int)(row * heightOffset + row * CellSize),
+                            CellSize,
+                            CellSize
                         );
 
                         var crop = new CroppedBitmap(source, rect);
 
+                        var panelRectangle = new System.Windows.Rect(
+                            column * CellSize + column * SeparationSize,
+                            row * CellSize + row * SeparationSize,
+                            CellSize,
+                            CellSize
+                        );
+
                         drawingContext.DrawImage(
                             crop,
-                            new System.Windows.Rect(
-                                column * cellSize,
-                                row * cellSize,
-                                cellSize,
-                                cellSize
-                            )
+                            panelRectangle
                         );
+
+                        if (column < Columns - 1) {
+                            //Vertical Line
+                            drawingContext.DrawLine(
+                                pen,
+                                new Point(
+                                    panelRectangle.X + CellSize + SeparationSize / 2d,
+                                    panelRectangle.Y
+                                ),
+                                new Point(
+                                    panelRectangle.X + CellSize + SeparationSize / 2d,
+                                    panelRectangle.Y + CellSize
+                                )
+                            );
+                        }
+
+                        if (row < Columns - 1) {
+                            //Horizontal Line
+                            drawingContext.DrawLine(
+                                pen,
+                                new Point(
+                                    panelRectangle.X,
+                                    panelRectangle.Y + CellSize + SeparationSize / 2d
+                                ),
+                                new Point(
+                                    panelRectangle.X + CellSize,
+                                    panelRectangle.Y + CellSize + SeparationSize / 2d
+                                )
+                            );
+                        }
                     }
                 }
             }
@@ -59,6 +92,18 @@ namespace NINA.ViewModel {
             bmp.Render(drawingVisual);
             bmp.Freeze();
             this.MosaicImage = bmp;
+        }
+
+        private int separationSize;
+
+        public int SeparationSize {
+            get {
+                return separationSize;
+            }
+            set {
+                separationSize = value;
+                RaisePropertyChanged();
+            }
         }
 
         private int columns;
