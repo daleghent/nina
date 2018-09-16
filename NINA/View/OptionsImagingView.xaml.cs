7d2bded7ab641bc2c6e663c9db50b9a6f2ef3788
Isbeorn <isbeorn86@googlemail.com>
2018-09-16 03:47:36 +0200

    PoC to drag & drop image patterns to textbox. Also double click in list to append pattern to textbox. ToDo: Do it in proper MVVM style.
    Former-commit-id: 19e7a8caed23e095ed8abd7f77155e965ae4e476
    Former-commit-id: 2d07f2b3ce01ad9ac08d2400f31a461c11ab8e04

--- NINA/View/OptionsImagingView.xaml.cs
+++ NINA/View/OptionsImagingView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using NINA.Model;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace NINA.View {
 
@@ -9,6 +12,41 @@ namespace NINA.View {
 
         public OptionsImagingView() {
             InitializeComponent();
+        }
+
+        private void TextBox_PreviewDragOver(object sender, DragEventArgs e) {
+            e.Handled = true;
+        }
+
+        private void TextBox_Drop(object sender, DragEventArgs e) {
+            var tb = sender as TextBox;
+            string tstring;
+            tstring = e.Data.GetData(DataFormats.StringFormat).ToString();
+            tb.Text += tstring;
+        }
+
+        private void TextBox_DragEnter(object sender, DragEventArgs e) {
+            e.Effects = DragDropEffects.Copy;
+        }
+
+        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            var item = sender as ListViewItem;
+            if (item != null) {
+                ImagePattern mySelectedItem = item.Content as ImagePattern;
+                if (mySelectedItem != null) {
+                    DragDrop.DoDragDrop(ImagePatternList, mySelectedItem.Key, DragDropEffects.Copy);
+                }
+            }
+        }
+
+        private void ListViewItem_PreviewMouseDoubleClick(object sender, RoutedEventArgs e) {
+            var item = sender as ListViewItem;
+            if (item != null) {
+                ImagePattern mySelectedItem = item.Content as ImagePattern;
+                if (mySelectedItem != null) {
+                    ImageFilePatternTextBox.Text += mySelectedItem.Key;
+                }
+            }
         }
     }
 }
