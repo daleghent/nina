26492a176e14842e9778fe0b65e5ccf655510ca9
Isbeorn <isbeorn86@googlemail.com>
2019-01-01 21:38:18 +0100

    Set Targetname when selecting a target in serachvm without searching again.
    Former-commit-id: de6f5ea649b4262fe022527738a4a08adbe4d5ac
    Former-commit-id: 5b8e58a894153043a1b1d10c2234255d402afe7f

--- NINA/ViewModel/DeepSkyObjectSearchVM.cs
+++ NINA/ViewModel/DeepSkyObjectSearchVM.cs
@@ -65,12 +65,14 @@ namespace NINA.ViewModel {
             get => targetName;
             set {
                 targetName = value;
-                if (!SkipSearch && TargetName.Length > 1) {
-                    targetSearchCts?.Cancel();
-                    targetSearchCts = new CancellationTokenSource();
-                    TargetSearchResult = NotifyTaskCompletion.Create(SearchDSOs(TargetName, targetSearchCts.Token));
-                } else {
-                    ShowPopup = false;
+                if (!SkipSearch) {
+                    if (TargetName.Length > 1) {
+                        targetSearchCts?.Cancel();
+                        targetSearchCts = new CancellationTokenSource();
+                        TargetSearchResult = NotifyTaskCompletion.Create(SearchDSOs(TargetName, targetSearchCts.Token));
+                    } else {
+                        ShowPopup = false;
+                    }
                 }
                 RaisePropertyChanged();
             }
@@ -101,6 +103,7 @@ namespace NINA.ViewModel {
             set {
                 selectedTargetSearchResult = value;
                 if (selectedTargetSearchResult != null) {
+                    this.SetTargetNameWithoutSearch(selectedTargetSearchResult.Column1);
                     Coordinates = new Coordinates(
                         Astrometry.HMSToDegrees(value.Column2),
                         Astrometry.DMSToDegrees(value.Column3),
