7f81a7bf8ed34421ba863886663806c00b1c199f
darkarchon <root.tarsonis@googlemail.com>
2019-01-14 21:24:39 +0100

    add some colors to DSOs
    Former-commit-id: 524c1dc4958799e3f3f83f950afa09d2ca3cf159
    Former-commit-id: 2583f597201001a364e78ca12642c35c2fbe0386

--- NINA/ViewModel/FramingAssistant/FramingDSO.cs
+++ NINA/ViewModel/FramingAssistant/FramingDSO.cs
@@ -2,6 +2,8 @@
 using NINA.Utility.Astrometry;
 using System.Drawing;
 using System.Linq;
+using Color = System.Drawing.Color;
+using Pen = System.Drawing.Pen;
 
 namespace NINA.ViewModel.FramingAssistant {
 
@@ -22,6 +24,7 @@ namespace NINA.ViewModel.FramingAssistant {
         /// <param name="dso">The DSO including its coordinates</param>
         /// <param name="viewport">The viewport of the offending DSO</param>
         public FramingDSO(DeepSkyObject dso, ViewportFoV viewport) {
+            dsoType = dso.DSOType;
             arcSecWidth = viewport.ArcSecWidth;
             arcSecHeight = viewport.ArcSecHeight;
 
@@ -83,25 +86,54 @@ namespace NINA.ViewModel.FramingAssistant {
         public string Name3 { get; }
 
         private static SolidBrush dsoFillColorBrush = new SolidBrush(Color.FromArgb(10, 255, 255, 255));
-        private static SolidBrush dsoFontColorBrush = new SolidBrush(Color.FromArgb(255, 255, 255, 255));
 
-        private static Pen dsoStrokePen = new Pen(Color.FromArgb(127, 255, 255, 255));
+        private static Pen galxyStrokePen = new Pen(Color.FromArgb(128, Color.Cyan));
+        private static SolidBrush galxyFontColorBrush = new SolidBrush(Color.Cyan);
 
-        private static Font fontdso = new Font("Segoe UI", 10, System.Drawing.FontStyle.Regular);
+        private static Pen nebulaStrokePen = new Pen(Color.FromArgb(128, Color.OrangeRed));
+        private static SolidBrush nebulaFontColorBrush = new SolidBrush(Color.OrangeRed);
+
+        private static Pen dsoDefaultStrokePen = new Pen(Color.FromArgb(127, 255, 255, 255));
+        private static SolidBrush dsoDefaultFontColorBrush = new SolidBrush(Color.FromArgb(255, 255, 255, 255));
+
+        private static Font dsoFont = new Font("Segoe UI", 10, System.Drawing.FontStyle.Regular);
+        private string dsoType;
 
         public void Draw(System.Drawing.Graphics g) {
+            Pen dsoPen;
+            SolidBrush dsoSolidBrush;
+            switch (dsoType) {
+                case "GALXY":
+                case "GALCL":
+                    dsoPen = galxyStrokePen;
+                    dsoSolidBrush = galxyFontColorBrush;
+                    break;
+
+                case "BRTNB":
+                case "PLNNB":
+                case "CL+NB":
+                    dsoPen = nebulaStrokePen;
+                    dsoSolidBrush = nebulaFontColorBrush;
+                    break;
+
+                default:
+                    dsoPen = dsoDefaultStrokePen;
+                    dsoSolidBrush = dsoDefaultFontColorBrush;
+                    break;
+            }
+
             g.FillEllipse(dsoFillColorBrush, (float)(this.CenterPoint.X - this.RadiusWidth), (float)(this.CenterPoint.Y - this.RadiusHeight),
                     (float)(this.RadiusWidth * 2), (float)(this.RadiusHeight * 2));
-            g.DrawEllipse(dsoStrokePen, (float)(this.CenterPoint.X - this.RadiusWidth), (float)(this.CenterPoint.Y - this.RadiusHeight),
+            g.DrawEllipse(dsoPen, (float)(this.CenterPoint.X - this.RadiusWidth), (float)(this.CenterPoint.Y - this.RadiusHeight),
                 (float)(this.RadiusWidth * 2), (float)(this.RadiusHeight * 2));
-            var size1 = g.MeasureString(this.Name1, fontdso);
-            g.DrawString(this.Name1, fontdso, dsoFontColorBrush, this.TextPosition.X - size1.Width / 2, (float)(this.TextPosition.Y));
+            var size1 = g.MeasureString(this.Name1, dsoFont);
+            g.DrawString(this.Name1, dsoFont, dsoSolidBrush, this.TextPosition.X - size1.Width / 2, (float)(this.TextPosition.Y));
             if (this.Name2 != null) {
-                var size2 = g.MeasureString(this.Name2, fontdso);
-                g.DrawString(this.Name2, fontdso, dsoFontColorBrush, this.TextPosition.X - size2.Width / 2, (float)(this.TextPosition.Y + size1.Height + 2));
+                var size2 = g.MeasureString(this.Name2, dsoFont);
+                g.DrawString(this.Name2, dsoFont, dsoSolidBrush, this.TextPosition.X - size2.Width / 2, (float)(this.TextPosition.Y + size1.Height + 2));
                 if (this.Name3 != null) {
-                    var size3 = g.MeasureString(this.Name3, fontdso);
-                    g.DrawString(this.Name3, fontdso, dsoFontColorBrush, this.TextPosition.X - size3.Width / 2, (float)(this.TextPosition.Y + size1.Height + 2 + size2.Height + 2));
+                    var size3 = g.MeasureString(this.Name3, dsoFont);
+                    g.DrawString(this.Name3, dsoFont, dsoSolidBrush, this.TextPosition.X - size3.Width / 2, (float)(this.TextPosition.Y + size1.Height + 2 + size2.Height + 2));
                 }
             }
         }
