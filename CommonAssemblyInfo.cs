70ad5d410282145aeb6420b7b11b086edd85595e
Isbeorn <isbeorn86@googlemail.com>
2019-02-15 09:37:28 +0100

    Added common assembly info to share across solution.
    Former-commit-id: 44817c333d945803b2f438b8941d7a78b688d70f
    Former-commit-id: 0bac64d7f947d975017d3e910689f522c3232c1b

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2017 - 2019 Stefan Berg and the N.I.N.A. contributors")]
+[assembly: AssemblyProduct("N.I.N.A. - Nighttime Imaging 'N' Astronomy")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+// Major Version Minor Version Build Number Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
+// as shown below: [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.8.0.1016")]
+[assembly: AssemblyFileVersion("1.8.0.1016")]
--- NINA/Properties/AssemblyInfo.cs
+++ NINA/Properties/AssemblyInfo.cs
@@ -31,11 +31,6 @@ using System.Windows;
 [assembly: AssemblyTitle("N.I.N.A. - Nighttime Imaging 'N' Astronomy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("N.I.N.A. - Nighttime Imaging 'N' Astronomy")]
-[assembly: AssemblyCopyright("Copyright ©  2016 - 2019 Stefan Berg and the N.I.N.A. contributors")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 #if DEBUG
 [assembly: InternalsVisibleTo("NINATest")]
 #endif
@@ -61,12 +56,3 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-
-// Version information for an assembly consists of the following four values:
-//
-// Major Version Minor Version Build Number Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
-// as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.0.1016")]
-[assembly: AssemblyFileVersion("1.8.0.1016")]
