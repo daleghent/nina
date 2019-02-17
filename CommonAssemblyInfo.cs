b846124e92b6ac7dadc8f2067e700fda04a06069
Isbeorn <isbeorn86@googlemail.com>
2019-02-17 05:59:10 +0100

    Version 1.8.0.2002
    Former-commit-id: a25ee3bd434eac91b8cc14eb823874b38baae019
    Former-commit-id: 42359157651a07a3f6482aa12241d91e1c7a3dc5

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -15,5 +15,5 @@
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.0.2001")]
-[assembly: AssemblyFileVersion("1.8.0.2001")]
+[assembly: AssemblyVersion("1.8.0.2002")]
+[assembly: AssemblyFileVersion("1.8.0.2002")]
