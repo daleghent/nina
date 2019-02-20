761e186c8da2f42475395b62ec28912719bd63dc
Isbeorn <isbeorn86@googlemail.com>
2019-02-20 22:02:40 +0100

    Added Version description to common assembly info
    Former-commit-id: 0b73ed2e8cca9de4f0dc123538053aec5291a43e
    Former-commit-id: 1e916c3a411bd1acb692c5b6d05446a7bb7ad1bf

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -9,11 +9,31 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Version information for an assembly consists of the following four values:
-//
-// Major Version Minor Version Build Number Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
-// as shown below: [assembly: AssemblyVersion("1.0.*")]
+//Versioning in N.I.N.A.
+//N.I.N.A. utilizes the versioning scheme MAJOR.MINOR.PATCH.CHANNEL|BUILDNRXXX
+//There is currently no automation used and versions are maintained manually.
+
+//MAJOR version increases for big changes, like changing technologies etc.
+
+//MINOR version will increase for every new released version
+
+//PATCH version is reserved to apply Hotfixes to a released versions
+
+//CHANNEL|BUILDNR will not be displayed for Released versions, as these are only used to identify Release, RC, Beta and Develop versions
+
+//CHANNEL consists of the following values:
+//1: Nightly
+//2: Beta
+//3: Release Candidate
+//9: Release
+
+//BUILDNR should be incremented each nightly build (only in develop, beta and rc versions) by using 3 digits.
+
+//Examples:
+//Release: 1.8.0.9001 (Displayed as "1.8")
+//Release: 1.8.1.9001 (Displayed as "1.8 HF1")
+//Release Candidate: 1.8.0.3001 (Displayed as "1.8 RC1")
+//Beta: 1.8.0.2004 (Displayed as "1.8 BETA004")
+//Develop: 1.8.0.1022 (Displayed as "1.8 NIGHTLY #022")
 [assembly: AssemblyVersion("1.8.0.2005")]
 [assembly: AssemblyFileVersion("1.8.0.2005")]
