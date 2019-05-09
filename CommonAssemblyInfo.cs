0b702d92571bb8e34896aaf19a638e14c9252b08
Isbeorn <isbeorn86@googlemail.com>
2019-05-10 01:40:03 +0200

    Version 1.9.0.1012
    Former-commit-id: 2263f503cc66c1333594044c94dff3ad16d5e2cb
    Former-commit-id: b9bc25495c9ee5b258db805590b500dc4f41e0bc

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Reflection;
 //Release Candidate: 1.8.0.3001 (Displayed as "1.8 RC1")
 //Beta: 1.8.0.2004 (Displayed as "1.8 BETA004")
 //Develop: 1.8.0.1022 (Displayed as "1.8 NIGHTLY #022")
-[assembly: AssemblyVersion("1.9.0.1011")]
-[assembly: AssemblyFileVersion("1.9.0.1011")]
+[assembly: AssemblyVersion("1.9.0.1012")]
+[assembly: AssemblyFileVersion("1.9.0.1012")]
