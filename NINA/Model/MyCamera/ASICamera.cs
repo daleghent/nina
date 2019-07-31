c5f3c6f46d7646b87cf9a56014bc04ec6f169810
Yannick Dutertre <yannickd@gmail.com>
2019-07-31 19:22:47 +0000

    Merged in cuiv/nina/1.9_ASIWorkaround (pull request #258)
    Workaround for ASI cooling getting stuck
    
    * AF when HFR increases
    
    * Formatting and Release notes
    
    * Localization
    
    * ASICamera gradual cooling fix
    
    * Remove the can set temperature condition
    
    
    Former-commit-id: 93f6b74864a64aa338903f464490b43d1f4154b0
    Former-commit-id: 18ea6eb26666eee91aacd667c485ca104f15e0f2

--- NINA/Model/MyCamera/ASICamera.cs
+++ NINA/Model/MyCamera/ASICamera.cs
@@ -142,7 +142,16 @@ namespace NINA.Model.MyCamera {
             }
             set {
                 if (CanSetTemperature) {
+                    //need to be an integer for ASI cameras
                     var nearest = (int)Math.Round(value);
+                    //for temperatures, automatically adjust to max or min accepted values
+                    var maxValue = GetControlMaxValue(ASICameraDll.ASI_CONTROL_TYPE.ASI_TARGET_TEMP);
+                    var minValue = GetControlMinValue(ASICameraDll.ASI_CONTROL_TYPE.ASI_TARGET_TEMP);
+                    if (nearest > maxValue) {
+                        nearest = maxValue;
+                    } else if (nearest < minValue) {
+                        nearest = minValue;
+                    }
                     if (SetControlValue(ASICameraDll.ASI_CONTROL_TYPE.ASI_TARGET_TEMP, nearest)) {
                         RaisePropertyChanged();
                     }
