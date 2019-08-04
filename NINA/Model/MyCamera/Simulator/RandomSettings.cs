0d74d887ab6f782e843118fc0299a8d6a83aec1c
Stefan B <isbeorn86@googlemail.com>
2019-08-04 12:37:04 +0000

    Merged in feature/customsim (pull request #262)
    Feature/customsim
    
    * Basic sky survey pulling for sim cam
    
    * Adding in some basic parameters for camera sim sky survey
    
    * Refactor of Simulator Camera to own namespace
    
    * Adjust focal length and pixel size for sky survey cam sim to enable plate solving
    
    
    Former-commit-id: 0bf24eb14fbe3f986d07e45f7626dd995f81c47f
    Former-commit-id: dda9ea2f6f08f04b87326541f485cbd17e5bbcb7

---
+++ NINA/Model/MyCamera/Simulator/RandomSettings.cs
@@ -0,0 +1,89 @@
+﻿#region "copyright"
+
+/*
+    Copyright © 2016 - 2019 Stefan Berg <isbeorn86+NINA@googlemail.com>
+
+    This file is part of N.I.N.A. - Nighttime Imaging 'N' Astronomy.
+
+    N.I.N.A. is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    N.I.N.A. is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with N.I.N.A..  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#endregion "copyright"
+
+using NINA.Utility;
+
+namespace NINA.Model.MyCamera.Simulator {
+
+    public class RandomSettings : BaseINPC {
+
+        public RandomSettings() {
+            ImageWidth = 640;
+            ImageHeight = 480;
+            ImageMean = 5000;
+            ImageStdDev = 100;
+        }
+
+        private object lockObj = new object();
+
+        private int imageWidth;
+
+        public int ImageWidth {
+            get => imageWidth;
+            set {
+                lock (lockObj) {
+                    imageWidth = value;
+                }
+                RaisePropertyChanged();
+                //RaisePropertyChanged(nameof(CameraXSize));
+            }
+        }
+
+        private int imageHeight;
+
+        public int ImageHeight {
+            get => imageHeight;
+            set {
+                lock (lockObj) {
+                    imageHeight = value;
+                }
+                RaisePropertyChanged();
+                //RaisePropertyChanged(nameof(CameraYSize));
+            }
+        }
+
+        private int imageMean;
+
+        public int ImageMean {
+            get => imageMean;
+            set {
+                lock (lockObj) {
+                    imageMean = value;
+                }
+                RaisePropertyChanged();
+            }
+        }
+
+        private int imageStdDev;
+
+        public int ImageStdDev {
+            get => imageStdDev;
+            set {
+                lock (lockObj) {
+                    imageStdDev = value;
+                }
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
