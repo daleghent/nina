46a21c9dc56e083b59045475b94cc1eb4f9f93f1
Igor von Nyssen <igor@vonnyssen.com>
2019-10-12 20:27:52 -0700

    found the flipflat on COM3
    Former-commit-id: a7ccebe792198ca178178a44f9f71f1699dddba7
    Former-commit-id: fd85648eee1adebe4eb4e3aba4e779d8768906c2

---
+++ NINA/Model/MyFlatDevice/AlnitakFlipFlatDevice.cs
@@ -0,0 +1,59 @@
+﻿using AlnitakAstrosystemsSDK;
+using NINA.Profile;
+using NINA.Utility;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NINA.Model.MyFlatDevice {
+
+    public class AlnitakFlatDevice : BaseINPC, IFlatDevice {
+        private bool _connected = false;
+        private LIBAlnitak.DeviceInfo Info;
+        private IProfileService profileService;
+
+        public AlnitakFlatDevice(string flatDevice, IProfileService profileService) {
+            this.profileService = profileService;
+            string[] flatDeviceInfo;
+
+            flatDeviceInfo = flatDevice.Split(';');
+            Info.Id = flatDeviceInfo[0];
+        }
+
+        public bool HasSetupDialog => false;
+
+        public string Id => Info.Id;
+
+        public string Name => Info.Model;
+
+        public string Category { get; } = "Alnitak Astrosystems";
+
+        public bool Connected {
+            get => _connected;
+            private set {
+                _connected = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Description => string.Format($"{Info.Model} ({Info.Id}) FWRev: {Info.FWrev}");
+
+        public string DriverInfo => string.Empty;
+
+        public string DriverVersion => "1.0";
+
+        public async Task<bool> Connect(CancellationToken token) {
+            return await Task.Run(() => {
+                Connected = LIBAlnitak.Ping();
+                return Connected;
+            });
+        }
+
+        public void Disconnect() {
+            Connected = false;
+            //actually disconnect;
+        }
+
+        public void SetupDialog() {
+        }
+    }
+}
---
+++ NINATest/LIBAlnitakTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+using AlnitakAstrosystemsSDK;
+
+namespace NINATest {
+
+    [TestFixture]
+    public class LIBAlnitakTest {
+
+        [Test]
+        public void TestPing() {
+            LIBAlnitak.Ping();
+        }
+    }
+}
