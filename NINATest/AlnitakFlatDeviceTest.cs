4686756aa9f59e0f88066e2bb6aab1eb40447c9c
Igor von Nyssen <igor@vonnyssen.com>
2019-10-14 21:20:34 -0700

    added first unit tests and a bit of mock scaffolding
    Former-commit-id: b4a29a684cbceeb750118f3db9905e0276b115c4
    Former-commit-id: a476508b3f2ed1a0953b76f285d3c151495df82c

--- NINATest/AlnitakFlatDeviceTest.cs
+++ NINATest/AlnitakFlatDeviceTest.cs
@@ -1,17 +1,47 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using Moq;
 using NINA.Model.MyFlatDevice;
 using NUnit.Framework;
+using System.Threading.Tasks;
 
 namespace NINATest {
 
     [TestFixture]
     public class AlnitakFlatDeviceTest {
+        private AlnitakFlatDevice _sut;
+        private Mock<ISerialPort> mockSerialPort;
+
+        [SetUp]
+        public async Task initAsync() {
+            _sut = new AlnitakFlatDevice("COM3");
+            mockSerialPort = new Mock<ISerialPort>();
+            _sut.SerialPort = mockSerialPort.Object;
+            mockSerialPort.Setup(m => m.ReadLine()).Returns("*P99OOO");
+            Assert.That(await _sut.Connect(new System.Threading.CancellationToken()), Is.EqualTo(true));
+        }
+
+        [TearDown]
+        public void Dispose() {
+            _sut.Disconnect();
+            Assert.AreEqual(false, _sut.Connected);
+        }
 
         [Test]
-        public async Task TestCoverStatus() {
-            AlnitakFlatDevice sut = new AlnitakFlatDevice("COM3");
-            _ = sut.MaxBrightness;
+        [TestCase(CoverState.NotOpenClosed, "*S99000")]
+        [TestCase(CoverState.Closed, "*S99001")]
+        [TestCase(CoverState.Open, "*S99002")]
+        [TestCase(CoverState.UNKNOWN, "*S99003")]
+        public void TestCoverStatus(CoverState coverState, string deviceResponse) {
+            mockSerialPort.Setup(m => m.ReadLine()).Returns(deviceResponse);
+            Assert.That(_sut.CoverState, Is.EqualTo(coverState));
+        }
+    }
+}
+
+/*            AlnitakFlatDevice sut = new AlnitakFlatDevice("COM3");
+            Assert.AreEqual(true, await sut.Connect(new System.Threading.CancellationToken()));
+            Assert.AreEqual(CoverState.Closed, sut.CoverState);
+            */
+/*            _ = sut.MaxBrightness;
             _ = sut.MinBrightness;
             _ = await sut.Connect(new System.Threading.CancellationToken());
             Console.WriteLine(sut.Description);
@@ -31,7 +61,4 @@ namespace NINATest {
             Console.WriteLine(sut.LightOn);
             Console.WriteLine(sut.LightOn);
             sut.LightOn = false;
-            //            sut.Disconnect();
-        }
-    }
-}
+            sut.Disconnect();*/
