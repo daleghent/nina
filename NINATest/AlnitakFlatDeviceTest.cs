ea1d4d137e42ec260b8760397b695be34c382472
Igor von Nyssen <igor@vonnyssen.com>
2019-10-14 21:43:56 -0700

    adding more unit tests
    Former-commit-id: 4442bebe40e29582fb528b43225e1659f735e112
    Former-commit-id: a8e2ae1423a10e22557a717896a3e11f7aa3ba6e

--- NINATest/AlnitakFlatDeviceTest.cs
+++ NINATest/AlnitakFlatDeviceTest.cs
@@ -30,9 +30,30 @@ namespace NINATest {
         [TestCase(CoverState.Closed, "*S99001")]
         [TestCase(CoverState.Open, "*S99002")]
         [TestCase(CoverState.UNKNOWN, "*S99003")]
+        [TestCase(CoverState.UNKNOWN, "garbage")]
         public void TestCoverStatus(CoverState coverState, string deviceResponse) {
             mockSerialPort.Setup(m => m.ReadLine()).Returns(deviceResponse);
             Assert.That(_sut.CoverState, Is.EqualTo(coverState));
+        }
+
+        [Test]
+        public void TestMinBrightness() {
+            Assert.That(_sut.MinBrightness, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestMaxBrightness() {
+            Assert.That(_sut.MaxBrightness, Is.EqualTo(255));
+        }
+
+        [Test]
+        [TestCase(0, "*J99000")]
+        [TestCase(255, "*J99255")]
+        [TestCase(99, "*J99099")]
+        [TestCase(0, "garbage")]
+        public void TestGetBrightness(int brightness, string deviceResponse) {
+            mockSerialPort.Setup(m => m.ReadLine()).Returns(deviceResponse);
+            Assert.That(_sut.Brightness, Is.EqualTo(brightness));
         }
     }
 }
