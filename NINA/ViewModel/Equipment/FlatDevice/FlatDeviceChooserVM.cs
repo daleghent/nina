b79d84f6ca734b6217c98171f11cce1c6e23f87c
Igor von Nyssen <igor@vonnyssen.com>
2019-10-20 19:35:07 -0700

    Looks to be working, need a image for the flat device though
    Former-commit-id: acdc0a83c073df1bbac2bed2d5db7ad99859c788
    Former-commit-id: a62d24322c84c4fbd084ba6b460f25ed6495cf4b

--- NINA/ViewModel/Equipment/FlatDevice/FlatDeviceChooserVM.cs
+++ NINA/ViewModel/Equipment/FlatDevice/FlatDeviceChooserVM.cs
@@ -7,37 +7,30 @@ using NINA.Profile;
 using NINA.Utility;
 using NINA.Utility.FlatDeviceSDKs.AlnitakSDK;
 
-namespace NINA.ViewModel.Equipment.FlatDevice
-{
-    internal class FlatDeviceChooserVM : EquipmentChooserVM
-    {
-        public FlatDeviceChooserVM(IProfileService profileService) : base(profileService)
-        {
+namespace NINA.ViewModel.Equipment.FlatDevice {
+
+    internal class FlatDeviceChooserVM : EquipmentChooserVM {
+
+        public FlatDeviceChooserVM(IProfileService profileService) : base(profileService) {
         }
 
-        public override void GetEquipment()
-        {
+        public override void GetEquipment() {
             Devices.Clear();
 
-            Devices.Add(new DummyDevice(Locale.Loc.Instance["LblNoFlatDevice"]));
+            Devices.Add(new DummyDevice(Locale.Loc.Instance["LblFlatDeviceNoDevice"]));
 
-            try
-            {
+            try {
                 Logger.Trace("Adding Alnitak Flat Devices");
                 List<string> alnitakList = AlnitakDevices.GetDevices();
 
-                if (alnitakList.Count > 0)
-                {
+                if (alnitakList.Count > 0) {
                     foreach (var device in alnitakList.Select(entry => new AlnitakFlatDevice(entry, profileService))
-                        .Where(device => !string.IsNullOrEmpty(device.Name)))
-                    {
+                        .Where(device => !string.IsNullOrEmpty(device.Name))) {
                         Logger.Debug($"Adding Alnitak flat device {device.Id} (as {device.Name})");
                         Devices.Add(device);
                     }
                 }
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 Logger.Error(ex);
             }
 
