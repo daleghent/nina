02e2cb3c86e783e8e532f1e5b22c0d75fb1c39a4
Igor von Nyssen <igor@vonnyssen.com>
2020-03-29 03:19:03 +0000

    Merged in igorvonnyssen/nina/develop (pull request #458)
    Test Coverage for ASCOM Focuser
    
    * adjusting fr new locale handling and button spacing
    
    * now waits for the thread to complete instead of creating/garbage collecting threads
    
    * updated serial device detection to use windoes management queries instead of probing ports with commands
    
    * look for shorter match
    
    * increased timeout to one second, fixed unit tests
    
    * generalized serial device communication to enable re-use for any serial device
    
    * fixed UI issue on flat panel view, added handling for darks, added safety for disposing serial ports
    
    * Alnitak Timeout Fix
    
    * updated method to return task<bool>
    
    * fix wizard trained values for cameras without a binning mode
    
    * changing to await task.delay
    
    * close cover before warming the camera, codemaid
    
    * Tests to cover ASCOM Focuser before refactoring
    
    * added debug logging for flats in sequence
    
    * updated according to CR comments
    
    * added logging
    
    
    Former-commit-id: ac0fe37eea7c2bd553d3b0d52ab53ff168b559cd
    Former-commit-id: 853214395fcbad21f8ef6f159dbc38de536d5992

--- NINA/ViewModel/SequenceVM.cs
+++ NINA/ViewModel/SequenceVM.cs
@@ -985,6 +985,9 @@ namespace NINA.ViewModel {
                         if (settings != null) {
                             _flatDeviceMediator.SetBrightness(settings.Brightness);
                             seq.ExposureTime = settings.Time;
+                            Logger.Debug($"Starting flat exposure with filter: {seq.FilterType?.Name}, binning: {seq.Binning}, gain: {seq.Gain}, panel brightness {settings.Brightness} and exposure time: {settings.Time}.");
+                        } else {
+                            Logger.Debug($"No settings found for filter: {seq.FilterType?.Name}, binning: {seq.Binning} and gain: {seq.Gain}.");
                         }
                     }
 
@@ -1017,6 +1020,9 @@ namespace NINA.ViewModel {
                         var settings = profileService.ActiveProfile.FlatDeviceSettings.GetBrightnessInfo(new FlatDeviceFilterSettingsKey(seq.FilterType.Name, seq.Binning, seq.Gain));
                         if (settings != null) {
                             seq.ExposureTime = settings.Time;
+                            Logger.Debug($"Starting dark flat exposure with filter: {seq.FilterType.Name}, binning: {seq.Binning}, gain: {seq.Gain} and exposure time: {settings.Time}.");
+                        } else {
+                            Logger.Debug($"No settings found for filter: {seq.FilterType.Name}, binning: {seq.Binning} and gain: {seq.Gain}.");
                         }
                     }
 
@@ -1160,7 +1166,7 @@ namespace NINA.ViewModel {
 
                 if (currentHfrTrend > (originalHfr * (1 + csl.AutoFocusAfterHFRChangeAmount / 100))) {
                     /* Trigger autofocus after HFR change */
-                    Logger.Debug($"Autofocus after HFR change has been triggered, as current HFR trend is {100*(currentHfrTrend / originalHfr - 1)}% higher compared to threshold of {csl.AutoFocusAfterHFRChangeAmount}%");
+                    Logger.Debug($"Autofocus after HFR change has been triggered, as current HFR trend is {100 * (currentHfrTrend / originalHfr - 1)}% higher compared to threshold of {csl.AutoFocusAfterHFRChangeAmount}%");
                     return true;
                 }
             }
