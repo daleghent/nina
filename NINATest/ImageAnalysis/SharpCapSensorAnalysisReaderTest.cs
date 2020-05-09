a9253b08ef457b92dae55ecc35f8111801a84b89
George Hilios <ghilios@gmail.com>
2020-05-09 12:33:07 +0000

    Merged in ghilios/nina/ghilios/sensor_analysis_fix (pull request #509)
    Change Sensor Analysis to use linear interpolation to deal with 0 cases
    
    
    Former-commit-id: 0bf0b9f12e5d1d82becf793f9620c71218424f4a
    Former-commit-id: b0a0b6bab71c1bf28670a7ce98ecf3db6a393b46

---
+++ NINATest/ImageAnalysis/SharpCapSensorAnalysisReaderTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using NINA.Utility.Astrometry;
+using NINA.Utility.ImageAnalysis;
+using NUnit.Framework;
+
+namespace NINATest.ImageAnalysis {
+    [TestFixture]
+    public class SharpCapSensorAnalysisReaderTest {
+        private string sensorAnalysisTestDataPath;
+        private ISharpCapSensorAnalysisReader reader;
+        private static string AA_SENSOR = "AA1600MPROTEC - MONO12";
+        private static string ZWO_SENSOR = "ZWO ASI1600MM Pro - MONO16";
+
+        [SetUp]
+        public void TestSetup() {
+            this.sensorAnalysisTestDataPath = Path.Combine(TestContext.CurrentContext.TestDirectory, @"ImageAnalysis\TestData");
+            this.reader = new DefaultSharpCapSensorAnalysisReader();
+        }
+
+        [Test]
+        public void ReadAllSensors_Test() {
+            var sensorAnalysisData = this.reader.Read(this.sensorAnalysisTestDataPath);
+            Assert.AreEqual(2, sensorAnalysisData.Count);
+            Assert.IsTrue(sensorAnalysisData.ContainsKey(AA_SENSOR));
+            Assert.IsTrue(sensorAnalysisData.ContainsKey(ZWO_SENSOR));
+        }
+
+        [Test]
+        public void MinGain_Test() {
+            var sensorAnalysisData = this.reader.Read(this.sensorAnalysisTestDataPath);
+            Assert.AreEqual(2, sensorAnalysisData.Count);
+            Assert.AreEqual(19346, sensorAnalysisData[AA_SENSOR].EstimateFullWellCapacity(100).EstimatedValue);
+            Assert.AreEqual(20952, sensorAnalysisData[ZWO_SENSOR].EstimateFullWellCapacity(0).EstimatedValue);
+            Assert.AreEqual(3.25515866, sensorAnalysisData[AA_SENSOR].EstimateReadNoise(100).EstimatedValue, 0.001);
+            Assert.AreEqual(3.510164, sensorAnalysisData[ZWO_SENSOR].EstimateReadNoise(0).EstimatedValue, 0.001);
+        }
+
+        [Test]
+        public void MaxGain_Test() {
+            var sensorAnalysisData = this.reader.Read(this.sensorAnalysisTestDataPath);
+            Assert.AreEqual(1152, sensorAnalysisData[AA_SENSOR].EstimateFullWellCapacity(2000).EstimatedValue);
+            Assert.AreEqual(64, sensorAnalysisData[ZWO_SENSOR].EstimateFullWellCapacity(500).EstimatedValue);
+            Assert.AreEqual(1.44073522, sensorAnalysisData[AA_SENSOR].EstimateReadNoise(2000).EstimatedValue, 0.001);
+            Assert.AreEqual(0.901244342, sensorAnalysisData[ZWO_SENSOR].EstimateReadNoise(500).EstimatedValue, 0.001);
+        }
+
+        [Test]
+        public void InBetweenGain_Test() {
+            var sensorAnalysisData = this.reader.Read(this.sensorAnalysisTestDataPath);
+            Assert.AreEqual(11518.5, sensorAnalysisData[AA_SENSOR].EstimateFullWellCapacity(174).EstimatedValue, 0.001);
+            Assert.AreEqual(10607.5, sensorAnalysisData[ZWO_SENSOR].EstimateFullWellCapacity(60).EstimatedValue, 0.001);
+            Assert.AreEqual(2.563265, sensorAnalysisData[AA_SENSOR].EstimateReadNoise(174).EstimatedValue, 0.001);
+            Assert.AreEqual(2.396112, sensorAnalysisData[ZWO_SENSOR].EstimateReadNoise(60).EstimatedValue, 0.001);
+        }
+    }
+}
