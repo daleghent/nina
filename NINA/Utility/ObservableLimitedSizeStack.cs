d53e03ffc7d64fba4bc57c9a57aee3dda1ec8731
George Hilios <ghilios@gmail.com>
2020-06-18 22:50:24 +0000

    Merged in ghilios/async_container_deadlock (pull request #597)
    Fix deadlock when using AsyncObservables by async posting messages to the dispatcher thread
    
    * Fix deadlock when using AsyncObservables by async posting messages to the dispatcher thread
    
    
    Former-commit-id: ed9770403972f979d81f182159b3590deb9de318
    Former-commit-id: be4d48fc9116f58892b4e261e3b1dbe81763053b

--- NINA/Utility/ObservableLimitedSizeStack.cs
+++ NINA/Utility/ObservableLimitedSizeStack.cs
@@ -44,7 +44,7 @@ namespace NINA.Utility {
                 RaiseCollectionChanged(e);
             } else {
                 // Raises the CollectionChanged event on the creator thread
-                _synchronizationContext.Send(RaiseCollectionChanged, e);
+                _synchronizationContext.Post(RaiseCollectionChanged, e);
             }
         }
 
@@ -60,7 +60,7 @@ namespace NINA.Utility {
                 RaisePropertyChanged(e);
             } else {
                 // Raises the PropertyChanged event on the creator thread
-                _synchronizationContext.Send(RaisePropertyChanged, e);
+                _synchronizationContext.Post(RaisePropertyChanged, e);
             }
         }
 
