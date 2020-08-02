6b54270af6cac2cec4f4fd6a1c9bd0d7145ea26d
Igor von Nyssen <igor@vonnyssen.com>
2020-08-02 18:05:06 +0000

    Merged in bugfix/BasicQuery_in_7_3 (pull request #661)
    Bugfix/BasicQuery in 7 3
    
    * Merge remote-tracking branch 'upstream/develop' into develop
    
    * Merge remote-tracking branch 'upstream/develop' into develop
    
    * Merge remote-tracking branch 'upstream/develop' into develop
    
    * Merge remote-tracking branch 'upstream/develop' into develop
    
    * Merge remote-tracking branch 'upstream/develop' into develop
    
    * Merge remote-tracking branch 'upstream/develop' into develop
    
    * Merge remote-tracking branch 'upstream/develop' into develop
    
    * Merge remote-tracking branch 'upstream/develop' into develop
    
    * Merge remote-tracking branch 'upstream/develop' into develop
    
    * changed the class to be C# 7.3 compliant
    
    
    Former-commit-id: 70db598df4c3e96d85a9176acc8a2efc2491f7d4
    Former-commit-id: cc7662b07f179da3a9faada66f5d559a20ab92bd

--- NINA/Utility/TcpRaw/BasicQuery.cs
+++ NINA/Utility/TcpRaw/BasicQuery.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 namespace NINA.Utility.TcpRaw {
 
     internal class BasicQuery {
-        public string Address { get; set; } = "localhost";
+        public string Address { get; set; }
 
-        public int Port { get; set; } = 0;
+        public int Port { get; set; }
 
-        public string Command { get; set; } = string.Empty;
+        public string Command { get; set; }
 
         public BasicQuery(string address, int port, string command) {
             Address = address;
@@ -19,32 +19,33 @@ namespace NINA.Utility.TcpRaw {
         }
 
         public async Task<string> SendQuery() {
-            using var client = new TcpClient();
-            try {
-                await client.ConnectAsync(Address, Port);
-            } catch (Exception ex) {
-                Logger.Error($"TcpRaw: Error connecting to {Address}:{Port}: {ex}");
-                throw ex;
+            using (var client = new TcpClient()) {
+                try {
+                    await client.ConnectAsync(Address, Port);
+                } catch (Exception ex) {
+                    Logger.Error($"TcpRaw: Error connecting to {Address}:{Port}: {ex}");
+                    throw;
+                }
+
+                Logger.Trace($"TcpRaw: Sending command: {ToLiteral(Command)}");
+                var data = Encoding.ASCII.GetBytes($"{Command}");
+                var stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+
+                var buffer = new byte[2048];
+                var length = stream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.ASCII.GetString(buffer, 0, length);
+
+                stream.Close();
+                client.Close();
+
+                Logger.Trace($"TcpRaw: Received message: {ToLiteral(response)}");
+
+                return response;
             }
-
-            Logger.Trace($"TcpRaw: Sending command: {ToLiteral(Command)}");
-            byte[] data = Encoding.ASCII.GetBytes($"{Command}");
-            var stream = client.GetStream();
-            stream.Write(data, 0, data.Length);
-
-            byte[] buffer = new byte[2048];
-            var length = stream.Read(buffer, 0, buffer.Length);
-            string response = Encoding.ASCII.GetString(buffer, 0, length);
-
-            stream.Close();
-            client.Close();
-
-            Logger.Trace($"TcpRaw: Received message: {ToLiteral(response)}");
-
-            return response;
         }
 
-        private string ToLiteral(string str) {
+        private static string ToLiteral(string str) {
             str = str.Replace("\r", @"\r").Replace("\n", @"\n").Replace("\t", @"\t");
 
             return str;
