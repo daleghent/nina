8e3c873b62930466d4f838d76d2cc1f4b93f48ca
Stefan Berg <isbeorn86@googlemail.com>
2020-10-31 19:47:36 +0100

    Version 1.10.2.2001
    Former-commit-id: 329a503167455a4a2dd392c0accc96931ee039a4
    Former-commit-id: e9ab32ee2083d1b5559827923f2c00905b7fa9c5

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Reflection;
 //Release Candidate: 1.8.0.3001 (Displayed as "1.8 RC1")
 //Beta: 1.8.0.2004 (Displayed as "1.8 BETA004")
 //Develop: 1.8.0.1022 (Displayed as "1.8 NIGHTLY #022")
-[assembly: AssemblyVersion("1.10.1.9001")]
-[assembly: AssemblyFileVersion("1.10.1.9001")]
+[assembly: AssemblyVersion("1.10.2.2001")]
+[assembly: AssemblyFileVersion("1.10.2.2001")]
