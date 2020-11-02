5b71247ae0aa29b20d1df77eb89d37652e73b8f0
Stefan B <isbeorn86@googlemail.com>
2020-11-02 17:43:56 +0000

    Merged in feature/sofaupdate (pull request #736)
    Feature/sofaupdate
    
    * update sofa source files
    
    * recompile SOFA dll with latest changes and platform version
    
    * Fix incorrect exports
    
    * recompile after fix
    
    * cleanup unused dll
    
    
    Former-commit-id: 4e6a7d998d85ad3cb20eef384a3780774d5a4238
    Former-commit-id: 49933d645d3430e667e5776fd4dc0b8203f2ac85

--- SOFA/SOFA/src/fk54z.c
+++ SOFA/SOFA/src/fk54z.c
@@ -1,38 +1,99 @@
 #include "sofa.h"
 
-double iauAnp(double a)
+void iauFk54z(double r2000, double d2000, double bepoch,
+              double *r1950, double *d1950,
+              double *dr1950, double *dd1950)
 /*
-**  - - - - - - -
-**   i a u A n p
-**  - - - - - - -
+**  - - - - - - - - -
+**   i a u F k 5 4 z
+**  - - - - - - - - -
 **
-**  Normalize angle into the range 0 <= a < 2pi.
+**  Convert a J2000.0 FK5 star position to B1950.0 FK4, assuming zero
+**  proper motion in FK5 and parallax.
 **
 **  This function is part of the International Astronomical Union's
-**  SOFA (Standards Of Fundamental Astronomy) software collection.
+**  SOFA (Standards of Fundamental Astronomy) software collection.
 **
-**  Status:  vector/matrix support function.
+**  Status:  support function.
 **
 **  Given:
-**     a        double     angle (radians)
+**     r2000,d2000    double   J2000.0 FK5 RA,Dec (rad)
+**     bepoch         double   Besselian epoch (e.g. 1950.0)
 **
-**  Returned (function value):
-**              double     angle in range 0-2pi
+**  Returned:
+**     r1950,d1950    double   B1950.0 FK4 RA,Dec (rad) at epoch BEPOCH
+**     dr1950,dd1950  double   B1950.0 FK4 proper motions (rad/trop.yr)
 **
-**  This revision:  2013 June 18
+**  Notes:
+**
+**  1) In contrast to the iauFk524  routine, here the FK5 proper
+**     motions, the parallax and the radial velocity are presumed zero.
+**
+**  2) This function converts a star position from the IAU 1976 FK5
+**    (Fricke) system to the former FK4 (Bessel-Newcomb) system, for
+**     cases such as distant radio sources where it is presumed there is
+**     zero parallax and no proper motion.  Because of the E-terms of
+**     aberration, such objects have (in general) non-zero proper motion
+**     in FK4, and the present routine returns those fictitious proper
+**     motions.
+**
+**  3) Conversion from B1950.0 FK4 to J2000.0 FK5 only is provided for.
+**     Conversions involving other equinoxes would require additional
+**     treatment for precession.
+**
+**  4) The position returned by this routine is in the B1950.0 FK4
+**     reference system but at Besselian epoch BEPOCH.  For comparison
+**     with catalogs the BEPOCH argument will frequently be 1950.0. (In
+**     this context the distinction between Besselian and Julian epoch
+**     is insignificant.)
+**
+**  5) The RA component of the returned (fictitious) proper motion is
+**     dRA/dt rather than cos(Dec)*dRA/dt.
+**
+**  Called:
+**     iauAnp       normalize angle into range 0 to 2pi
+**     iauC2s       p-vector to spherical
+**     iauFk524     FK4 to FK5
+**     iauS2c       spherical to p-vector
+**
+**  This revision:   2018 December 5
 **
 **  SOFA release 2020-07-21
 **
 **  Copyright (C) 2020 IAU SOFA Board.  See notes at end.
 */
 {
-   double w;
+   double r, d, pr, pd, px, rv, p[3], w, v[3];
+   int i;
 
 
-   w = fmod(a, D2PI);
-   if (w < 0) w += D2PI;
+/* FK5 equinox J2000.0 to FK4 equinox B1950.0. */
+   iauFk524(r2000, d2000, 0.0, 0.0, 0.0, 0.0,
+            &r, &d, &pr, &pd, &px, &rv);
 
-   return w;
+/* Spherical to Cartesian. */
+   iauS2c(r, d, p);
+
+/* Fictitious proper motion (radians per year). */
+   v[0] = - pr*p[1] - pd*cos(r)*sin(d);
+   v[1] =   pr*p[0] - pd*sin(r)*sin(d);
+   v[2] =             pd*cos(d);
+
+/* Apply the motion. */
+   w = bepoch - 1950.0;
+   for ( i = 0; i < 3; i++ ) {
+      p[i] += w*v[i];
+   }
+
+/* Cartesian to spherical. */
+   iauC2s(p, &w, d1950);
+   *r1950 = iauAnp(w);
+
+/* Fictitious proper motion. */
+   *dr1950 = pr;
+   *dd1950 = pd;
+
+/* Finished. */
 
 /*----------------------------------------------------------------------
 **
