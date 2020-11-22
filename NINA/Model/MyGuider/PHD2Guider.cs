8a1538a5689624fc4f2a857226fbe34559b6dcd5
Stefan Berg <isbeorn86@googlemail.com>
2020-11-22 01:30:21 +0100

    Wait for message (up to 10 seconds) when starting the phd2 process
    Former-commit-id: cd4d148c0414c51d8174874533ae2fc2670c89dc
    Former-commit-id: b610add628f1b7b9f44383a737bbc41abc384175

--- NINA/Model/MyGuider/PHD2Guider.cs
+++ NINA/Model/MyGuider/PHD2Guider.cs
@@ -182,7 +182,6 @@ namespace NINA.Model.MyGuider {
                 var resp = await SendMessage(PHD2EventId.GET_PIXEL_SCALE, PHD2Methods.GET_PIXEL_SCALE);
                 if (resp.result != null)
                     PixelScale = double.Parse(resp.result.ToString().Replace(",", "."), CultureInfo.InvariantCulture);
-
             } catch (OperationCanceledException) {
             } catch (Exception ex) {
                 Logger.Error(ex);
@@ -509,9 +508,20 @@ namespace NINA.Model.MyGuider {
                     var process = Process.Start(profileService.ActiveProfile.GuiderSettings.PHD2Path);
                     process?.WaitForInputIdle();
 
-                    await Task.Delay(1500);
+                    await Task.Delay(2000);
 
-                    return true;
+                    //Try to read the appstate and retry for 5 times. On slow systems the startup of phd can take a couple of seconds.
+                    string appState = string.Empty;
+                    int retries = 5;
+                    do {
+                        try {
+                            retries--;
+                            appState = await GetAppState(2000);
+                        } catch (Exception) {
+                        }
+                    } while (string.IsNullOrEmpty(appState) && retries > 0);
+
+                    return !string.IsNullOrEmpty(appState);
                 }
             } catch (FileNotFoundException ex) {
                 Logger.Error(Locale.Loc.Instance["LblPhd2PathNotFound"]);
