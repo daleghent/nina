defee1bd6b0031125df9db9f4198a98d14630ef7
Nick Hardy <n.a.hardy@gmail.com>
2021-03-14 12:08:50 +0000

    Merged in HfrMinorChange (pull request #873)
    HfrMinorChange
    
    * Changed rms in history to total rms text
    
    * fixed history
    
    * Merge branch 'develop' into HfrMinorChange
    
    * a few minor changes to the history
    
    * Added mechanical rotator position
    
    * Merged Isbeorn/nina:develop into NickHardy/nina:HfrMinorChange

--- NINA/ViewModel/ImageHistory/ImageHistoryPoint.cs
+++ NINA/ViewModel/ImageHistory/ImageHistoryPoint.cs
@@ -59,8 +59,11 @@ namespace NINA.ViewModel.ImageHistory {
             if (imageSavedEventArgs.MetaData?.Target != null) {
                 Target = imageSavedEventArgs.MetaData.Target;
             }
-            if (imageSavedEventArgs.MetaData?.Focuser != null && !double.IsNaN((double)imageSavedEventArgs.MetaData?.Focuser?.Temperature)) {
-                Temperature = imageSavedEventArgs.MetaData.Focuser.Temperature;
+            if (imageSavedEventArgs.MetaData?.Focuser != null) {
+                if (!double.IsNaN((double)imageSavedEventArgs.MetaData?.Focuser?.Temperature))
+                    Temperature = imageSavedEventArgs.MetaData.Focuser.Temperature;
+                if ((bool)(imageSavedEventArgs.MetaData?.Focuser?.Position.HasValue))
+                    FocuserPosition = (int)imageSavedEventArgs.MetaData.Focuser.Position;
             } else if (imageSavedEventArgs.MetaData?.WeatherData != null && !double.IsNaN((double)imageSavedEventArgs.MetaData?.WeatherData?.Temperature)) {
                 Temperature = imageSavedEventArgs.MetaData.WeatherData.Temperature;
             }
@@ -69,6 +72,15 @@ namespace NINA.ViewModel.ImageHistory {
                 RecordedRMS = imageSavedEventArgs.MetaData.Image.RecordedRMS;
                 Rms = RecordedRMS.Total * RecordedRMS.Scale;
                 RmsText = RecordedRMS.TotalText;
+            }
+
+            if (imageSavedEventArgs.MetaData?.Rotator != null) {
+                if (!double.IsNaN((double)imageSavedEventArgs.MetaData?.Rotator?.Position)) {
+                    RotatorPosition = (double)imageSavedEventArgs.MetaData.Rotator.Position;
+                }
+                if (!double.IsNaN((double)imageSavedEventArgs.MetaData?.Rotator?.MechanicalPosition)) {
+                    RotatorMechanicalPosition = (double)imageSavedEventArgs.MetaData.Rotator.MechanicalPosition;
+                }
             }
         }
 
@@ -102,32 +114,42 @@ namespace NINA.ViewModel.ImageHistory {
         public string Type { get; private set; }
 
         public TargetParameter Target { get; private set; }
+
+        public int FocuserPosition { get; private set; } = 0;
+        public double RotatorPosition { get; private set; } = 0.0;
+        public double RotatorMechanicalPosition { get; private set; } = 0.0;
     }
 
     public sealed class ImageHistoryPointMap : ClassMap<ImageHistoryPoint> {
 
         public ImageHistoryPointMap() {
-            Map(m => m.Id);
-            Map(m => m.Target.Name);
-            Map(m => m.Target.Coordinates.RA);
-            Map(m => m.Target.Coordinates.Dec);
-            Map(m => m.Target.Rotation);
-            Map(m => m.Filename);
-            Map(m => m.dateTime);
-            Map(m => m.Temperature);
-            Map(m => m.Duration);
-            Map(m => m.Filter);
-            Map(m => m.HFR);
-            Map(m => m.Stars);
-            Map(m => m.Median);
-            Map(m => m.Mean);
-            Map(m => m.StDev);
-            Map(m => m.MAD);
-            Map(m => m.RecordedRMS.RA);
-            Map(m => m.RecordedRMS.Dec);
-            Map(m => m.RecordedRMS.Total);
-            Map(m => m.RecordedRMS.Scale);
-            Map(m => m.RecordedRMS.TotalText);
+            Map(m => m.Id).Index(1);
+            Map(m => m.Target.Name).Index(2).Name("Target Name");
+            Map(m => m.Target.Coordinates.RA).Index(3).Name("Target Ra");
+            Map(m => m.Target.Coordinates.Dec).Index(4).Name("Target Dec");
+            Map(m => m.Target.Rotation).Optional().Index(5).Name("Target Rotation");
+            Map(m => m.Filename).Index(6);
+            Map(m => m.dateTime).Index(7);
+            Map(m => m.Temperature).Optional().Index(8);
+            Map(m => m.Duration).Index(9);
+            Map(m => m.Filter).Index(10);
+            Map(m => m.HFR).Index(11);
+            Map(m => m.Stars).Index(12);
+            Map(m => m.Median).Index(13);
+            Map(m => m.Mean).Index(14);
+            Map(m => m.StDev).Index(15);
+            Map(m => m.MAD).Index(16);
+            Map(m => m.RecordedRMS.TotalText).Optional().Index(17).Name("Rms");
+            Map(m => m.RecordedRMS.RA).Optional().Index(18).Name("Rms Ra");
+            Map(m => m.RecordedRMS.Dec).Optional().Index(19).Name("Rms Dec");
+            Map(m => m.RecordedRMS.Total).Optional().Index(20).Name("Rms Total");
+            Map(m => m.RecordedRMS.Scale).Optional().Index(21).Name("Rms Scale");
+            Map(m => m.RotatorPosition).Optional().Index(22).Name("Rotator Position");
+            Map(m => m.RotatorMechanicalPosition).Optional().Index(23).Name("Rotator MechanicalPosition");
+            Map(m => m.FocuserPosition).Optional().Index(24).Name("Focuser Position");
+            Map(m => m.AutoFocusPoint.OldPosition).Optional().Index(25).Name("AutoFocus Old Position");
+            Map(m => m.AutoFocusPoint.NewPosition).Optional().Index(26).Name("AutoFocus New Position");
+            Map(m => m.AutoFocusPoint.Time).Index(27).Optional().Name("AutoFocus Time");
         }
     }
 }
