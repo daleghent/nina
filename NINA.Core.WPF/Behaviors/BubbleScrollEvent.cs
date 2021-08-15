51f3c015be4bf4f306e69ce4b145c976174b5e99
Stefan B <isbeorn86@googlemail.com>
2021-08-15 11:52:21 +0000

    Merged in feature/markdown (pull request #1049)
    Feature/markdown
    
    * Add MdXaml.dll to setup
    
    * Fix horizontal scrollbar direction when clicking
    
    * Add remaining font properties to markdownstyle
    
    * Remove margin from markdown lists
    
    * Support opening of hyperlinks in markdown and further styling
    
    * Additionaly markdown styling
    
    * add missing package
    
    * Markdown for plugin long descriptions

---
+++ NINA.Core.WPF/Behaviors/BubbleScrollEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace NINA.WPF.Base.Behaviors {
+
+    public sealed class BubbleScrollEvent : Behavior<UIElement> {
+
+        protected override void OnAttached() {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseWheel += AssociatedObject_PreviewMouseWheel;
+        }
+
+        protected override void OnDetaching() {
+            AssociatedObject.PreviewMouseWheel -= AssociatedObject_PreviewMouseWheel;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_PreviewMouseWheel(object sender, MouseWheelEventArgs e) {
+            e.Handled = true;
+            var e2 = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+            e2.RoutedEvent = UIElement.MouseWheelEvent;
+            AssociatedObject.RaiseEvent(e2);
+        }
+    }
+}
