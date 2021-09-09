4db01a54fe24863a44315680a5a25fa47c394409
George Hilios <ghilios@gmail.com>
2021-09-09 22:18:01 +0000

    Merged in ghilios/cooler_timeout_billing (pull request #1071)
    Bill cooler timeout only if sufficient progress isn't being made
    
    * Bill cooler timeout only if sufficient progress isn't being made
    
    * Bill cooler timeout only if sufficient progress isn't being made

--- NINA.Core.WPF/ViewModel/Equipment/Camera/CameraVM.cs
+++ NINA.Core.WPF/ViewModel/Equipment/Camera/CameraVM.cs
@@ -183,6 +183,7 @@ namespace NINA.WPF.Base.ViewModel.Equipment.Camera {
             try {
                 double currentTemp = Cam.Temperature;
                 var totalDeltaTemp = Math.Abs(currentTemp - temperature);
+                var cooling = currentTemp < temperature;
 
                 if (duration > TimeSpan.Zero) {
                     // Stepped temp change
@@ -213,17 +214,22 @@ namespace NINA.WPF.Base.ViewModel.Equipment.Camera {
                 // Wait for final step
                 var timeout = TimeSpan.FromMinutes(1);
                 var idleTime = TimeSpan.Zero;
-                while (Math.Abs(currentTemp - temperature) > 1) {
+                var progressThreshold = 0.5d;
+                var previousTemperature = currentTemp;
+                while ((cooling && currentTemp > (temperature + 1)) || (!cooling && currentTemp < (temperature - 1))) {
                     var progressTemp = Math.Abs(currentTemp - temperature);
                     progress.Report((totalDeltaTemp - progressTemp) / totalDeltaTemp);
 
                     var t = await CoreUtil.Wait(TimeSpan.FromSeconds(5), ct);
                     currentTemp = Cam.Temperature;
-
                     var coolerPower = Cam.CoolerPower;
-                    if (coolerPower < 1 || coolerPower > 99) {
-                        //if cooler power is 100% and target lower it over a minute cannot reach target
-                        //if cooler power is 0% and target is higher over a minute it cannot reach target
+                    var deltaTemp = currentTemp - previousTemperature;
+                    previousTemperature = currentTemp;
+                    if (coolerPower < 1 && deltaTemp < progressThreshold) {
+                        // if cooler power is 0% and temperature is not increasing enough over a 5 second period (0.5C) then bill towards the timeout
+                        idleTime += t;
+                    } else if (coolerPower > 99 && deltaTemp > -progressThreshold) {
+                        // if cooler power is 100% and temperature is not decreasing enough over a 5 second period (0.5C) then bill towards the timeout
                         idleTime += t;
                     } else {
                         idleTime = TimeSpan.Zero;
@@ -233,7 +239,7 @@ namespace NINA.WPF.Base.ViewModel.Equipment.Camera {
                         throw new CannotReachTargetTemperatureException();
                     }
                 }
-            } catch (OperationCanceledException ex) {
+            } catch (OperationCanceledException) {
                 if (Cam != null) {
                     Cam.TemperatureSetPoint = Cam.Temperature;
                 }
