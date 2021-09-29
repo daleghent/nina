31310324251f8455e3f962a64d5d37a0ecb581fc
Stefan Berg <isbeorn86@googlemail.com>
2021-09-29 23:41:19 +0200

    Version 1.11.0.1154

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Reflection;
 //Release Candidate: 1.8.0.3001 (Displayed as "1.8 RC1")
 //Beta: 1.8.0.2004 (Displayed as "1.8 BETA004")
 //Develop: 1.8.0.1022 (Displayed as "1.8 NIGHTLY #022")
-[assembly: AssemblyVersion("1.11.0.1153")]
-[assembly: AssemblyFileVersion("1.11.0.1153")]
-[assembly: AssemblyInformationalVersion("1.11.0.1153-nightly")]
+[assembly: AssemblyVersion("1.11.0.1154")]
+[assembly: AssemblyFileVersion("1.11.0.1154")]
+[assembly: AssemblyInformationalVersion("1.11.0.1154-nightly")]
