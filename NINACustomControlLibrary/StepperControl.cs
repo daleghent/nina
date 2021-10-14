2dec5ee9f530bc4c6af245156a9082e6b96af81e
Stefan Berg <isbeorn86@googlemail.com>
2021-10-14 23:17:01 +0200

    Replace flat wizard settings sliders with steppers to be more in line with the rest of the ui style

--- NINACustomControlLibrary/StepperControl.cs
+++ NINACustomControlLibrary/StepperControl.cs
@@ -195,4 +195,182 @@ namespace NINACustomControlLibrary {
             }
         }
     }
+
+    /// <summary> Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project. Add
+    /// this XmlNamespace attribute to the root element of the markup file where it is to be used:
+    ///
+    /// xmlns:MyNamespace="clr-namespace:NINACustomControlLibrary"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project. Add
+    /// this XmlNamespace attribute to the root element of the markup file where it is to be used:
+    ///
+    /// xmlns:MyNamespace="clr-namespace:NINACustomControlLibrary;assembly=NINACustomControlLibrary"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives to
+    /// this project and Rebuild to avoid compilation errors:
+    ///
+    /// Right click on the target project in the Solution Explorer and "Add
+    /// Reference"->"Projects"->[Select this project]
+    ///
+    ///
+    /// Step 2) Go ahead and use your control in the XAML file.
+    ///
+    /// <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    [TemplatePart(Name = "PART_TextBox", Type = typeof(TextBox))]
+    [TemplatePart(Name = "PART_Decrement", Type = typeof(Button))]
+    [TemplatePart(Name = "PART_Increment", Type = typeof(Button))]
+    public class PrecisionStepperControl : UserControl {
+
+        static PrecisionStepperControl() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PrecisionStepperControl), new FrameworkPropertyMetadata(typeof(PrecisionStepperControl)));
+        }
+
+        public static readonly DependencyProperty ButtonForegroundBrushProperty =
+           DependencyProperty.Register(nameof(ButtonForegroundBrush), typeof(Brush), typeof(PrecisionStepperControl), new UIPropertyMetadata(new SolidColorBrush(Colors.White)));
+
+        public Brush ButtonForegroundBrush {
+            get {
+                return (Brush)GetValue(ButtonForegroundBrushProperty);
+            }
+            set {
+                SetValue(ButtonForegroundBrushProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty AddSVGProperty =
+           DependencyProperty.Register(nameof(AddSVG), typeof(Geometry), typeof(PrecisionStepperControl), new UIPropertyMetadata(null));
+
+        public Geometry AddSVG {
+            get {
+                return (Geometry)GetValue(AddSVGProperty);
+            }
+            set {
+                SetValue(AddSVGProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty SubstractSVGProperty =
+           DependencyProperty.Register(nameof(SubstractSVG), typeof(Geometry), typeof(PrecisionStepperControl), new UIPropertyMetadata(null));
+
+        public Geometry SubstractSVG {
+            get {
+                return (Geometry)GetValue(SubstractSVGProperty);
+            }
+            set {
+                SetValue(SubstractSVGProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+           DependencyProperty.Register(nameof(Value), typeof(double), typeof(PrecisionStepperControl), new UIPropertyMetadata(0.0d));
+
+        public double Value {
+            get {
+                return (double)GetValue(ValueProperty);
+            }
+            set {
+                SetValue(ValueProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty MinValueProperty =
+           DependencyProperty.Register(nameof(MinValue), typeof(double), typeof(PrecisionStepperControl), new UIPropertyMetadata(double.MinValue));
+
+        public double MinValue {
+            get {
+                return (double)GetValue(MinValueProperty);
+            }
+            set {
+                SetValue(MinValueProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty MaxValueProperty =
+           DependencyProperty.Register(nameof(MaxValue), typeof(double), typeof(PrecisionStepperControl), new UIPropertyMetadata(double.MaxValue));
+
+        public double MaxValue {
+            get {
+                return (double)GetValue(MaxValueProperty);
+            }
+            set {
+                SetValue(MaxValueProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty StepSizeProperty =
+           DependencyProperty.Register(nameof(StepSize), typeof(double), typeof(PrecisionStepperControl), new UIPropertyMetadata(1.0d));
+
+        public double StepSize {
+            get {
+                return (double)GetValue(StepSizeProperty);
+            }
+            set {
+                SetValue(StepSizeProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Customizing Hook to overwrite the default Textbox. "Value" still needs proper binding for the increment/decrement buttons to work properly
+        /// </summary>
+        public FrameworkElement InnerContent {
+            get { return (FrameworkElement)GetValue(InnerContentProperty); }
+            set { SetValue(InnerContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty InnerContentProperty =
+            DependencyProperty.Register(nameof(InnerContent), typeof(FrameworkElement), typeof(PrecisionStepperControl), new UIPropertyMetadata(null));
+
+        public override void OnApplyTemplate() {
+            base.OnApplyTemplate();
+            var button = GetTemplateChild("PART_Increment") as Button;
+            if (button != null) {
+                button.Click += Button_PART_Increment_Click;
+            }
+
+            button = GetTemplateChild("PART_Decrement") as Button;
+            if (button != null) {
+                button.Click += Button_PART_Decrement_Click;
+            }
+
+            var tb = GetTemplateChild("PART_Textbox") as TextBox;
+            if (tb != null) {
+                tb.LostFocus += PART_TextBox_LostFocus;
+            }
+
+            var cc = GetTemplateChild("PART_ContentControl") as ContentControl;
+            if (cc != null) {
+                cc.LostFocus += PART_TextBox_LostFocus;
+            }
+        }
+
+        private void Button_PART_Increment_Click(object sender, RoutedEventArgs e) {
+            if (Value + StepSize <= MaxValue) {
+                Value += StepSize;
+            } else {
+                Value = MaxValue;
+            }
+        }
+
+        private void Button_PART_Decrement_Click(object sender, RoutedEventArgs e) {
+            if (Value - StepSize >= MinValue) {
+                Value -= StepSize;
+            } else {
+                Value = MinValue;
+            }
+        }
+
+        private void PART_TextBox_LostFocus(object sender, RoutedEventArgs e) {
+            if (Value < MinValue) {
+                Value = MinValue;
+            }
+            if (Value > MaxValue) {
+                Value = MaxValue;
+            }
+        }
+    }
 }
