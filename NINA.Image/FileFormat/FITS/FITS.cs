3c45e7d39c293489034db11c20d5377914aac602
Stefan Berg <isbeorn86@googlemail.com>
2022-07-01 16:27:47 +0200

    Guard against Access Violation Exception in CFitsio

--- NINA.Image/FileFormat/FITS/FITS.cs
+++ NINA.Image/FileFormat/FITS/FITS.cs
@@ -18,6 +18,9 @@ using NINA.Image.Interfaces;
 using System;
 using System.Globalization;
 using System.IO;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -35,74 +38,94 @@ namespace NINA.Image.FileFormat.FITS {
             this.Data = new FITSData(data);
         }
 
-        public static unsafe Task<IImageData> Load(Uri filePath, bool isBayered, IImageDataFactory imageDataFactory, CancellationToken ct) {
-            return Task.Run<IImageData>(() => {
-                IntPtr fitsPtr = IntPtr.Zero;
-                try {
-                    var bytes = File.ReadAllBytes(filePath.LocalPath);
 
-                    fixed (byte* p = bytes) {
+        public static Task<IImageData> Load(Uri filePath, bool isBayered, IImageDataFactory imageDataFactory, CancellationToken ct) {
+            return Task.Run<IImageData>(() => LoadInternal(filePath, isBayered, imageDataFactory, ct), ct);
+        }
 
-                        IntPtr buffer = (IntPtr)p;
-                        UIntPtr size = new UIntPtr((uint)bytes.Length);
-                        UIntPtr deltaSize = UIntPtr.Zero;
+        [HandleProcessCorruptedStateExceptions, SecurityCritical]
+        private static IImageData LoadInternal(Uri filePath, bool isBayered, IImageDataFactory imageDataFactory, CancellationToken ct) {
+            IntPtr fitsPtr = IntPtr.Zero;
+            IntPtr buffer = IntPtr.Zero;
+            try {
+                var bytes = File.ReadAllBytes(filePath.LocalPath);
 
-                        CfitsioNative.fits_open_memory(out fitsPtr, string.Empty, CfitsioNative.IOMODE.READONLY, ref buffer, ref size, ref deltaSize, IntPtr.Zero, out var status);                        
-                        CfitsioNative.CheckStatus("fits_open_memory", status);
+                buffer = Marshal.AllocHGlobal(bytes.Length);
+                Marshal.Copy(bytes, 0, buffer, bytes.Length);
 
-                        var dimensions = CfitsioNative.fits_read_key_long(fitsPtr, "NAXIS");
-                        if (dimensions > 2) {
-                            Logger.Warning("Reading debayered FITS images not supported. Reading the first 2 axes to get a monochrome image");
-                        }
+                UIntPtr size = new UIntPtr((uint)bytes.Length);
+                UIntPtr deltaSize = UIntPtr.Zero;
 
-                        var width = (int)CfitsioNative.fits_read_key_long(fitsPtr, "NAXIS1");
-                        var height = (int)CfitsioNative.fits_read_key_long(fitsPtr, "NAXIS2");
-                        var bitPix = (CfitsioNative.BITPIX)(int)CfitsioNative.fits_read_key_long(fitsPtr, "BITPIX");
-                        var pixels = CfitsioNative.read_ushort_pixels(fitsPtr, bitPix, 2, width * height);
+                CfitsioNative.fits_open_memory(out fitsPtr, string.Empty, CfitsioNative.IOMODE.READONLY, ref buffer, ref size, ref deltaSize, IntPtr.Zero, out var status);
+                CfitsioNative.CheckStatus("fits_open_memory", status);
 
-                        //Translate nom.tam.fits into N.I.N.A. FITSHeader
-                        FITSHeader header = new FITSHeader(width, height);
-                        CfitsioNative.fits_get_hdrspace(fitsPtr, out var numKeywords, out var numMoreKeywords, out status);
-                        CfitsioNative.CheckStatus("fits_get_hdrspace", status);
-                        for (int headerIdx = 1; headerIdx <= numKeywords; ++headerIdx) {
-                            CfitsioNative.fits_read_keyn(fitsPtr, headerIdx, out var keyName, out var keyValue, out var keyComment);
+                var dimensions = CfitsioNative.fits_read_key_long(fitsPtr, "NAXIS");
+                if (dimensions > 2) {
+                    Logger.Warning("Reading debayered FITS images not supported. Reading the first 2 axes to get a monochrome image");
+                }
 
-                            if (string.IsNullOrEmpty(keyValue) || keyName.Equals("COMMENT") || keyName.Equals("HISTORY")) {
-                                continue;
-                            }
+                var width = (int)CfitsioNative.fits_read_key_long(fitsPtr, "NAXIS1");
+                var height = (int)CfitsioNative.fits_read_key_long(fitsPtr, "NAXIS2");
+                var bitPix = (CfitsioNative.BITPIX)(int)CfitsioNative.fits_read_key_long(fitsPtr, "BITPIX");
+                var pixels = CfitsioNative.read_ushort_pixels(fitsPtr, bitPix, 2, width * height);
 
-                            if (keyValue.Equals("T")) {
-                                header.Add(keyName, true, keyComment);
-                            } else if (keyValue.Equals("F")) {
-                                header.Add(keyName, false, keyComment);
-                            } else if (keyValue.Contains(".")) {
-                                if (double.TryParse(keyValue, NumberStyles.Number, CultureInfo.InvariantCulture, out var value)) {
-                                    header.Add(keyName, value, keyComment);
-                                }
-                            } else {
-                                if (int.TryParse(keyValue, NumberStyles.Number, CultureInfo.InvariantCulture, out var value)) {
-                                    header.Add(keyName, value, keyComment);
-                                } else {
-                                    // Treat as a string
-                                    header.Add(keyName, keyValue, keyComment);
-                                }
-                            }
-                        }
+                //Translate nom.tam.fits into N.I.N.A. FITSHeader
+                FITSHeader header = new FITSHeader(width, height);
+                CfitsioNative.fits_get_hdrspace(fitsPtr, out var numKeywords, out var numMoreKeywords, out status);
+                CfitsioNative.CheckStatus("fits_get_hdrspace", status);
+                for (int headerIdx = 1; headerIdx <= numKeywords; ++headerIdx) {
+                    CfitsioNative.fits_read_keyn(fitsPtr, headerIdx, out var keyName, out var keyValue, out var keyComment);
 
-                        var metaData = new ImageMetaData();
-                        try {
-                            metaData = header.ExtractMetaData();
-                        } catch (Exception ex) {
-                            Logger.Error(ex.Message);
-                        }
-                        return imageDataFactory.CreateBaseImageData(pixels, width, height, 16, isBayered, metaData);
+                    if (string.IsNullOrEmpty(keyValue) || keyName.Equals("COMMENT") || keyName.Equals("HISTORY")) {
+                        continue;
                     }
-                } finally {
-                    if (fitsPtr != IntPtr.Zero) {
-                        CfitsioNative.fits_close_file(fitsPtr, out var status);
+
+                    if (keyValue.Equals("T")) {
+                        header.Add(keyName, true, keyComment);
+                    } else if (keyValue.Equals("F")) {
+                        header.Add(keyName, false, keyComment);
+                    } else if (keyValue.Contains(".")) {
+                        if (double.TryParse(keyValue, NumberStyles.Number, CultureInfo.InvariantCulture, out var value)) {
+                            header.Add(keyName, value, keyComment);
+                        }
+                    } else {
+                        if (int.TryParse(keyValue, NumberStyles.Number, CultureInfo.InvariantCulture, out var value)) {
+                            header.Add(keyName, value, keyComment);
+                        } else {
+                            // Treat as a string
+                            header.Add(keyName, keyValue, keyComment);
+                        }
                     }
                 }
-            }, ct);
+
+                var metaData = new ImageMetaData();
+                try {
+                    metaData = header.ExtractMetaData();
+                } catch (Exception ex) {
+                    Logger.Error(ex.Message);
+                }
+                return imageDataFactory.CreateBaseImageData(pixels, width, height, 16, isBayered, metaData);
+
+            } catch (AccessViolationException ex) {
+                Logger.Error($"{nameof(FITS)} - Access Violation Exception occurred during cfitsio load!", ex);
+                // Finally blocks are not executed after corrupted state exception
+                if (fitsPtr != IntPtr.Zero) {
+                    try {
+                        CfitsioNative.fits_close_file(fitsPtr, out var status);
+                    } catch(Exception) { }                    
+                }
+                if (buffer != IntPtr.Zero) {
+                    Marshal.FreeHGlobal(buffer);
+                }
+                throw new Exception($"Unable to load FITS file from {filePath.LocalPath}");
+            } finally {
+                if (fitsPtr != IntPtr.Zero) {
+                    CfitsioNative.fits_close_file(fitsPtr, out var status);
+                }
+                if (buffer != IntPtr.Zero) {
+                    Marshal.FreeHGlobal(buffer);
+                }
+            }
         }
 
         public FITSHeader Header { get; }
