10e86b93e43ca557832037687f5c06e62c676b1b
Stefan Berg <isbeorn86@googlemail.com>
2022-12-11 22:53:54 +0100

    Version 3.0 NIGHTLY #002

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -49,9 +49,9 @@ using System.Reflection;
 //Release Candidate: 1.8.0.3001 (Displayed as "1.8 RC1")
 //Beta: 1.8.0.2004 (Displayed as "1.8 BETA004")
 //Develop: 1.8.0.1022 (Displayed as "1.8 NIGHTLY #022")
-[assembly: AssemblyVersion("3.0.0.1001")]
-[assembly: AssemblyFileVersion("3.0.0.1001")]
-[assembly: AssemblyInformationalVersion("3.0.0.1001-nightly")]
+[assembly: AssemblyVersion("3.0.0.1002")]
+[assembly: AssemblyFileVersion("3.0.0.1002")]
+[assembly: AssemblyInformationalVersion("3.0.0.1002-nightly")]
 
 // The PluginMinimumApplicationVersion specifies the minimum application version the plugin needs to specify in order to be compatible
 // Plugins that have a version specified that is below this version are marked as incompatible
