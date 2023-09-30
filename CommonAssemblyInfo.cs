63949915a700947599811dc44c4ff45e6e01dbc2
Stefan Berg <isbeorn86@googlemail.com>
2023-09-30 04:11:13 +0200

    Version 2.3 HF1 RC002

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -49,6 +49,6 @@ using System.Reflection;
 //Release Candidate: 1.8.0.3001 (Displayed as "1.8 RC1")
 //Beta: 1.8.0.2004 (Displayed as "1.8 BETA004")
 //Develop: 1.8.0.1022 (Displayed as "1.8 NIGHTLY #022")
-[assembly: AssemblyVersion("2.3.1.3001")]
-[assembly: AssemblyFileVersion("2.3.1.3001")]
+[assembly: AssemblyVersion("2.3.1.3002")]
+[assembly: AssemblyFileVersion("2.3.1.3002")]
 //[assembly: AssemblyInformationalVersion("2.3.0.9001")]
